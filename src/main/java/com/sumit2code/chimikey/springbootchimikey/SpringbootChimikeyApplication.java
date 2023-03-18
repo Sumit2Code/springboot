e4c0dfc3f845c0d32fb178b1b2dcf55acaedd454
sumit <sumit.kumar.shahuoffice@gmail.com>
2023-03-18 14:01:31 +0530

    Configuration changes done to work in tomcat server

--- src/main/java/com/sumit2code/chimikey/springbootchimikey/SpringbootChimikeyApplication.java
+++ src/main/java/com/sumit2code/chimikey/springbootchimikey/SpringbootChimikeyApplication.java
@@ -2,11 +2,18 @@ package com.sumit2code.chimikey.springbootchimikey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.sumit2code.chimikey")
-public class SpringbootChimikeyApplication {
+public class SpringbootChimikeyApplication extends SpringBootServletInitializer {
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(SpringbootChimikeyApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootChimikeyApplication.class, args);
